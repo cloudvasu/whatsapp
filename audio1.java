dd8d59ae5bb221755c04bfeca7245bf4b680d224
cloudvasu <159446508+cloudvasu@users.noreply.github.com>
2024-02-19 20:35:31 +0530

    changeaudio1.java

--- audio1.java
+++ audio1.java
@@ -6,8 +6,6 @@ public class Main{
           if(j!=i)  
            System.out.print(j);  
            else  
-            System.out.print("*");  
-   }  
-   System.out.println("");  
-   }  
+            System.out.print
+             
     }}  
